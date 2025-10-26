8bf7fb4dcc23c30d6f5228db32745d0ad4c9f1e6
hirokii97 <basket100713hiroki.ch@gmail.com>
2025-10-26 15:55:47 +0900

    ヘッダーにリンク追加

--- lib/components/layout/Layout.tsx
+++ lib/components/layout/Layout.tsx
@@ -8,9 +8,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="bg-gray-50 min-h-screen font-sans">
       <header className="px-4 py-6 border-b border-gray-200 bg-white shadow-sm">
-        <h1 className="text-3xl font-extrabold text-gray-800 tracking-tighter">
-          KATARO
-        </h1>
+        <a href="/" className="block w-30">
+          <h1 className="text-3xl font-extrabold text-gray-800 tracking-tighter">
+            KATARO
+          </h1>
+        </a>
       </header>
 
       <main className="container mx-auto px-4 py-6">{children}</main>
